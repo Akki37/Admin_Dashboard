14e011aa52c029c60f3c2de5ab1b286d73b951ad
Vikas Goswami <vikasg224@gmail.com>
2021-03-18 13:45:50 +0530

    App

--- src/App.js
+++ src/App.js
@@ -8,23 +8,7 @@ import MeanStack from "./MeanStack";
 import FullStack from "./FullStack";
 
 function App(props) {
-  
-    // let button = document.getElementsByClassName("btn")
-    // console.log(button)
-    // for (let i = 0; i < button.length; i++) {
-    //     button[i].addEventListener("click", function() {
-    //     let current = document.getElementsByClassName("active")
-    //     console.log(current)
-    //      if(current.length>0){
-    //       console.log(true);
-    //       current[0].className= current[0].className.replace(" active", "");
-    //      }
-    //     this.className += " active";
-    //     console.log(this);
-    //     })}
-  
-
-  
+   
   return (
     <div className="App">
       <Route path="/:dev/:id" component={ShowDetails}/>
